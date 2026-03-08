215d40f83cbe94e24396d13a12f0d62949e123ab
marshallwi11 <williamoscarmarshall@outlook.com>
2026-03-08 23:39:54 +0000

    update_26

--- app/page.tsx
+++ app/page.tsx
@@ -361,23 +361,23 @@ export default function Home() {
                 {/* Releases card */}
                 <div className="play-card" onClick={(e) => e.stopPropagation()} style={{marginTop:"clamp(16px,4vw,32px)"}}>
                   <div className="play-card-header">
-                    <span className="play-block-title">RELEASES</span>
-                    <span className="play-block-subtitle">ALBUMS</span>
+                    <span className="play-block-title">ALBUMS</span>
+                    <span className="play-block-subtitle">RELEASED</span>
                   </div>
                   <div className="releases-list">
                     <div className="release-row">
                       <span className="play-block-body">THE MARSHALL MAFIA</span>
-                      <span className="play-block-subtitle release-tag">EP&nbsp;&nbsp;&nbsp;RELEASED</span>
+                      <span className="play-block-subtitle release-tag">EP</span>
                     </div>
                     <hr className="play-card-divider" />
                     <div className="release-row">
                       <span className="play-block-body">THE MARSHALL MAFIA</span>
-                      <span className="play-block-subtitle release-tag">ALBUM&nbsp;&nbsp;&nbsp;RELEASED</span>
+                      <span className="play-block-subtitle release-tag">ALBUM</span>
                     </div>
                     <hr className="play-card-divider" />
                     <div className="release-row">
                       <span className="play-block-body">THE MARSHALL MAFIA</span>
-                      <span className="play-block-subtitle release-tag">SOUNDTRACKS&nbsp;&nbsp;&nbsp;RELEASED</span>
+                      <span className="play-block-subtitle release-tag">SOUNDTRACKS</span>
                     </div>
                   </div>
                 </div>
